2bb1a2a761198cfb9ce19cc448b061ba0f7938ed
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-02-02 14:24:10 +0900

    ユーザ登録画面ビューの調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,22 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  # def new
+  #   @item = Item.new
+  # end
+
+  # def create
+  #   Item.create(items_params)
+  #   redirect_to '/'
+  # end
+
+
+  # private
+  # def items_params
+  #   params.require(:item).permit()
+  # end
+
+
+
 end
