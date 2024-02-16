af4ee5c8f42330c1243c211831fb36ebf09e6fb0
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-02-16 16:39:49 +0900

    ユーザモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :email, :encrypted_password,
-            :first_name, :last_name, :first_name_kana,
+  validates :nickname, :first_name, :last_name, :first_name_kana,
             :last_name_kana, :birth_day, presence: true
             
 end
