4a55e26af08bdd4ab92d322170218bf36ad37dc7
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-01 13:41:20 +0900

    商品出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,21 @@
 class ItemsController < ApplicationController
-  # def index
-  #   @items = Item.all
-  # end
+  before_action :authenticate_user!, only: [:new, :create]
 
-  # def new
-  #   @item = Item.new
-  # end
+  def index
+    # @items = Item.all
+  end
 
-  # def create
-  #   Item.create(items_params)
-  #   redirect_to '/'
-  # end
+  def new
+    @item = Item.new
+  end
 
-  # private
-  # def items_params
-  #   params.require(:item).permit()
-  # end
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user, :item_name, :item_description, :item_category_id, :item_condition_id, :burden_of_shipping_id, :prefecture_id, :delivery_time_id, :item_price )
+  end
 end
