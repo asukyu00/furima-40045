7ad9f90415a6e0c91eb035b522d7733f3797cb23
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-01 13:00:41 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :user
+    validates :item_name
+    validates :item_description
+    validates :item_category_id
+    validates :item_condition_id
+    validates :burden_of_shipping_id
+    validates :prefecture_id
+    validates :delivery_time_id
+    validates :item_price 
+  end
+
 end
