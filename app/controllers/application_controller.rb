f18e145216d103c395899005bae0db55d856e5ec
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-01 14:46:43 +0900

    ユーザ管理機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,8 +9,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day])
-  end
 end
