11aa0a595b3366d4131a7f6eea48fff7d242af7f
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-01 14:13:19 +0900

    prefectureのActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
   belongs_to :user
   has_one_attached :image
 
@@ -14,5 +17,7 @@ class Item < ApplicationRecord
     validates :delivery_time_id
     validates :item_price 
   end
+
+  validates :prefecture_id, numericality: { other_than: 1 } 
 
 end
