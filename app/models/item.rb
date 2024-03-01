a61c97518493f61d92cebdb28edeb4f310360c8a
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-01 14:22:14 +0900

    categoryのActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,22 +2,26 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  belongs_to :category
 
   belongs_to :user
   has_one_attached :image
 
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+    validates :item_category_id
+  end  
+
   with_options presence: true do
     validates :user
-    validates :item_name
-    validates :item_description
-    validates :item_category_id
-    validates :item_condition_id
-    validates :burden_of_shipping_id
+    # validates :item_name
+    # validates :item_description
+    # validates :item_category_id
+    # validates :item_condition_id
+    # validates :burden_of_shipping_id
     validates :prefecture_id
     validates :delivery_time_id
-    validates :item_price 
+    # validates :item_price 
   end
-
-  validates :prefecture_id, numericality: { other_than: 1 } 
 
 end
