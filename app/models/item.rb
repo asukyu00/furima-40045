3f5ba2788a2d867957b4e94e22f6be6ca4c97921
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-13 15:25:14 +0900

    バリデーションの再設定および価格登録範囲の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,11 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :shipping_costs
   belongs_to :shipping_date
+  has_one_attached :image
 
   belongs_to :user
- 
+
+  validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}
 
   with_options numericality: { other_than: 1 } do
     validates :prefecture_id
@@ -16,19 +18,19 @@ class Item < ApplicationRecord
     validates :item_condition_id
     validates :burden_of_shipping_id
     validates :delivery_time_id
-    validates :image
   end  
 
   with_options presence: true do
     validates :user
-    # validates :item_name
-    # validates :item_description
-    # validates :item_category_id
-    # validates :item_condition_id
-    # validates :burden_of_shipping_id
-    # validates :prefecture_id
-    # validates :delivery_time_id
-    # validates :item_price 
+    validates :item_name
+    validates :item_description
+    validates :item_category_id
+    validates :item_condition_id
+    validates :burden_of_shipping_id
+    validates :prefecture_id
+    validates :delivery_time_id
+    validates :item_price
+    validates :image
   end
 
 end
