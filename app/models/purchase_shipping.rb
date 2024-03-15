5da16943b72097e9086b9a8d9da4f03668b76f6e
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-15 13:40:01 +0900

    データを各テーブルに保存

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -15,4 +15,8 @@ class PurchaseShipping
 
   validates :prefecture_id, numericality: { other_than: 1 }
 
+  def save
+    purchase = purchase.create(user_id: user_id, item_id: item_id)
+    Shipping.create(post_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
 end
