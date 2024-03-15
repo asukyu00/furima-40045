2a826ee7b34cb2082494a2b162f3ff0c216b7f4c
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-15 12:44:44 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
+  has_one :purchase
 
   validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,2 +1,7 @@
 class Shipping < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  
+  belongs_to :purchase
+
 end
