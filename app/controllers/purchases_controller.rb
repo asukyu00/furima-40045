741ffd0e8ffa74887a1c6948bbcb7cfbe5c2238d
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-15 13:46:28 +0900

    コントローラ調整

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,26 @@
 class PurchasesController < ApplicationController
+  
+  def index
+  end
+
+  def new
+    @purchase_shipping = PurchaseShipping.new
+  end
+
+  def create
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping).permit(:post_code, :prefecture_id, :municipalities, :block, :building, :phone_number).merge(user_id: current_user.id)
+  end
+
 end
