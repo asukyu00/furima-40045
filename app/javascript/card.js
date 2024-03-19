e32e5a8cb2c6aa07149eb45d0fbcd98e4b6f3fc3
asukyu00 <108255414+asukyu00@users.noreply.github.com>
2024-03-19 21:21:45 +0900

    環境変数調整

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,6 @@
 const pay = () => {
   const publicKey = gon.public_key
-  const payjp = Payjp('pk_test_c10f8b134bb1e36b54fe2100')// PAY.JPテスト公開鍵
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
